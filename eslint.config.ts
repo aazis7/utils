53a3b661b7eec55fe1ad58d3dedfed104abc435d
aazis7 <azis190407@gmail.com>
2025-08-13 15:46:52 +0000

    chore: add eslint, update dependency resolution to module, adjust the typescript config

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "node_modules", "__tests__"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {},
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    ...tseslint.configs.recommended,
+  },
+]);
