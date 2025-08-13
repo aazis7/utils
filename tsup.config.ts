db5025cf48d8c43cc472588149977273011a5811
aazis7 <azis190407@gmail.com>
2025-08-13 16:52:34 +0000

    refactor: specify and disabled splitting build, reorganize types exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig, type Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
-  entry: ['src/**'],
+  entry: [
+    'src/index.ts',
+    'src/string/index.ts',
+    'src/number/index.ts',
+    'src/time/index.ts',
+    'src/object/index.ts',
+    'src/validate/index.ts',
+  ],
   outDir: 'dist',
   clean: true,
-  minify: true,
   format: ['cjs', 'esm'],
   dts: true,
-  splitting: true,
+  splitting: false,
   treeshake: true,
-  sourcemap: false,
   target: 'es2022',
   bundle: false,
+  minify: true,
   ...options,
 }))
